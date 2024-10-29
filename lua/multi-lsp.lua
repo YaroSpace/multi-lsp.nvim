53f524238dcf679dfd4d2b356bc4ce37eff18349
Yaro <yaro@mail.ru>
2024-10-29 18:12:35 +0300

    added Signature help

--- lua/multi-lsp.lua
+++ lua/multi-lsp.lua
@@ -2,35 +2,65 @@ local log = require('vim.lsp.log')
 local util = require("vim.lsp.util")
 
 local M = {}
+---
+---@class MultiLspConfig
+---@field silent boolean supress notifications if no results from Lsp server
+---@field filter { method: string } default 'all' | name of Lsp server to use
+M.config = {
+	border = "rounded",
+	silent = true,
+	filter = {
+    ['textDocument/hover'] = 'ruby_lsp',
+    ['textDocument/definition'] = 'all',
+    ['textDocument/signatureHelp'] = 'all'
+	},
+	focus_signature_mapping = '<C-j>'
+}
 
-function M.hover(_, result, ctx, config)
+---@param ctx lsp.HandlerContext
+---@param method string LspMethod
+---@return integer buf 
+---@return vim.lsp.Client|nil client
+---@return integer clients_no
+---@return integer visits_count
+local function get_context(ctx, method)
   local buf = vim.fn.bufnr()
   local client = vim.lsp.get_client_by_id(ctx.client_id)
-  local clients_no = vim.tbl_count(vim.lsp.get_clients({ bufnr = buf, method = 'textDocument/hover' }))
+  local clients_no = vim.tbl_count(vim.lsp.get_clients({ bufnr = buf, method = method }))
 
-  local visits_count = vim.F.npcall(vim.api.nvim_buf_get_var, buf, 'visits_count')
+  local visits_count = vim.F.npcall(vim.api.nvim_buf_get_var, buf, method .. '_visits_count')
   visits_count = visits_count and (visits_count + 1) or 1
-  vim.api.nvim_buf_set_var(buf, 'visits_count', visits_count)
+  vim.api.nvim_buf_set_var(buf, method .. '_visits_count', visits_count)
 
-  local stored_contents = vim.F.npcall(vim.api.nvim_buf_get_var, buf, 'stored_contents')
+  return buf, client, clients_no, visits_count
+end
+
+local function clear_context(buf, method)
+  vim.api.nvim_buf_del_var(buf, method .. '_visits_count')
+  vim.api.nvim_buf_del_var(buf, method .. '_result')
+end
+
+---@param _ lsp.ResponseError?
+---@param result lsp.Hover
+---@param ctx lsp.HandlerContext
+---@param config table Configuration table.
+function M.hover(_, result, ctx, config)
+  local method = 'textDocument/hover'
+  local buf, client, clients_no, visits_count = get_context(ctx, method)
+
+  local filter = M.config.filter[method]
+  if filter and filter ~= client.name then return end
 
 	local format = "markdown"
 	local contents ---@type string[]
+  local stored_contents = vim.F.npcall(vim.api.nvim_buf_get_var, buf, method .. '_result')
 
-	config = config or {}
+	config = vim.tbl_deep_extend("keep", config or {}, M.config)
 	config.focus_id = ctx.method
 
-	config = vim.tbl_deep_extend("force", config, {
-		border = "rounded",
-		silent = true,
-	})
-
   local function show_hover_float()
-	  if visits_count < clients_no or (not stored_contents) or #stored_contents == 0 then return end
-
-    vim.api.nvim_buf_del_var(buf, 'visits_count')
-    vim.api.nvim_buf_del_var(buf, 'stored_contents')
-
+	  if not stored_contents or #stored_contents == 0 or visits_count < clients_no then return end
+    clear_context(buf, method)
 		return util.open_floating_preview(stored_contents, format, config)
 	end
 
@@ -42,10 +72,6 @@ function M.hover(_, result, ctx, config)
 			vim.notify("No information available")
 		end
 	  return show_hover_float()
-	end
-
-	if clients_no > 1 then
-		result.contents.value = '*' .. client.name .. '*' .. "\n\n" .. result.contents.value
 	end
 
 	if type(result.contents) == "table" and result.contents.kind == "plaintext" then
@@ -60,55 +86,60 @@ function M.hover(_, result, ctx, config)
 		return show_hover_float()
 	end
 
-  if stored_contents  then table.insert(contents, 1, '---------\n') end
+	if clients_no > 1 then table.insert(contents, 1, string.format('*%s*', client.name)) end
+  if stored_contents then table.insert(stored_contents, '---------') end
+
   stored_contents = stored_contents and vim.list_extend(stored_contents, contents) or contents
-  vim.api.nvim_buf_set_var(buf, 'stored_contents', stored_contents)
+  vim.api.nvim_buf_set_var(buf, method .. '_result', stored_contents)
 
 	return show_hover_float()
 end
 
+---@param locations table
+local function remove_duplicate_locations(locations)
+	if #locations == 2 then
+		local uri_1 = locations[1].targetUri or locations[1].uri
+		local uri_2 = locations[2].targetUri or locations[2].uri
+		local start_line_1 = locations[1].range and locations[1].range.start.line or locations[1].targetRange.start.line
+		local start_line_2 = locations[2].range and locations[2].range.start.line or locations[2].targetRange.start.line
+
+		if uri_1 == uri_2 and start_line_1 == start_line_2 then
+			table.remove(locations, 1)
+		end
+	end
+	return locations
+end
 --- Jumps to a location. Used as a handler for multiple LSP methods.
 ---@param _ nil not used
 ---@param result (table) result of LSP method; a location or a list of locations.
 ---@param ctx (lsp.HandlerContext) table containing the context of the request, including the method
 ---@param config? vim.lsp.LocationOpts
 ---(`textDocument/definition` can return `Location` or `Location[]`
-M.location_handler = function(_, result, ctx, config)
-  local buf = vim.fn.bufnr()
-  local client = vim.lsp.get_client_by_id(ctx.client_id)
-  local clients_no = vim.tbl_count(vim.lsp.get_clients({ bufnr = buf, method = 'textDocument/definition' }))
+M.location = function(_, result, ctx, config)
+  local method = 'textDocument/definition'
+  local buf, client, clients_no, visits_count = get_context(ctx, method)
 
-  local visits_count = vim.F.npcall(vim.api.nvim_buf_get_var, buf, 'visits_count')
-  visits_count = visits_count and (visits_count + 1) or 1
-  vim.api.nvim_buf_set_var(buf, 'visits_count', visits_count)
+  local filter = M.config.filter[method]
+  if filter and filter ~= client.name then return end
 
-  config = config or {}
-
-  local slr = vim.F.npcall(vim.api.nvim_buf_get_var, buf, 'stored_location_result')
+  local stored_result = vim.F.npcall(vim.api.nvim_buf_get_var, buf, method .. '_result')
+	config = vim.tbl_deep_extend("keep", config or {}, M.config)
 
   local function process_locations()
-    if visits_count < clients_no or (not slr) or #slr == 0 then return end
-    if #slr == 2 then --- remove duplicate location
-      local uri_1 = slr[1].targetUri or slr[1].uri
-      local uri_2 = slr[2].targetUri or slr[2].uri
-      local start_line_1 = slr[1].range and slr[1].range.start.line or slr[1].targetRange.start.line
-      local start_line_2 = slr[2].range and slr[2].range.start.line or slr[2].targetRange.start.line
+    if not stored_result or #stored_result == 0 or visits_count < clients_no then return end
+    clear_context(buf, method)
 
-      if uri_1 == uri_2 and start_line_1 == start_line_2 then table.remove(slr, 1) end
-    end
-
-    vim.api.nvim_buf_del_var(buf, 'visits_count')
-    vim.api.nvim_buf_del_var(buf, 'stored_location_result')
+    stored_result = remove_duplicate_locations(stored_result)
 
     local title = 'LSP locations'
-    local items = util.locations_to_items(slr, client.offset_encoding)
+    local items = util.locations_to_items(stored_result, client.offset_encoding)
 
     if config.on_list then
       assert(vim.is_callable(config.on_list), 'on_list is not a function')
       return config.on_list({ title = title, items = items })
     end
 
-    if #slr == 1 then
+    if #stored_result == 1 then
       return util.jump_to_location(result[1], client.offset_encoding, config.reuse_win)
     end
 
@@ -123,28 +154,103 @@ M.location_handler = function(_, result, ctx, config)
 
   if result == nil or vim.tbl_isempty(result) then
     log.info(ctx.method, 'No location found')
-    slr = {}
     return process_locations()
   end
 
-  if not vim.islist(result) then
-    result = { result }
-  end
+  if not vim.islist(result) then result = { result } end
 
-  vim.print(string.format('Client name %s, Locations# %s', client.name, #result))
-  slr = slr and vim.list_extend(slr, result) or result
-  vim.api.nvim_buf_set_var(buf, 'stored_location_result', slr)
+  stored_result = stored_result and vim.list_extend(stored_result, result) or result
+  vim.api.nvim_buf_set_var(buf, method .. '_result', stored_result)
 
   process_locations()
 end
 
+local function set_mapping_for_signature_window(current_buf, signature_buf, signature_win)
+  vim.keymap.set('i', '<C-j>', '', {
+    buffer = current_buf,
+    callback = function()
+      vim.api.nvim_create_autocmd('BufWinLeave', {
+        buffer = signature_buf,
+        once = true,
+        callback = function()
+          vim.keymap.del('i', M.config.focus_signature_mapping, { buffer = current_buf })
+        end
+      })
+      vim.api.nvim_set_current_win(signature_win)
+    end
+  })
+end
+
+---@param _ lsp.ResponseError?
+---@param result lsp.SignatureHelp  Response from the language server
+---@param ctx lsp.HandlerContext Client context
+---@param config table Configuration table.
+---     - border:     (default=nil)
+function M.signature_help(_, result, ctx, config)
+  local method = 'textDocument/signatureHelp'
+  local buf, client, clients_no, visits_count = get_context(ctx, method)
+
+  local filter = M.config.filter[method]
+  if filter and filter ~= client.name then return end
+
+	local lines, hl
+  local stored_result = vim.F.npcall(vim.api.nvim_buf_get_var, buf, method .. '_result')
+
+	config = vim.tbl_deep_extend("keep", config or {}, M.config)
+	config.focus_id = ctx.method
+
+	local function show_signature()
+    if not stored_result or #stored_result == 0 or visits_count < clients_no then return end
+    clear_context(buf, method)
+
+    local fbuf, fwin = util.open_floating_preview(stored_result, 'markdown', config)
+    if hl then
+      -- Highlight the second line if the signature is wrapped in a Markdown code block.
+      local line = vim.startswith(stored_result[1], '```') and 1 or 0
+      vim.api.nvim_buf_add_highlight(fbuf, -1, 'LspSignatureActiveParameter', line, unpack(hl))
+    end
+
+    set_mapping_for_signature_window(buf, fbuf, fwin)
+
+    return fbuf, fwin
+	end
+
+  if buf ~= ctx.bufnr then return show_signature() end -- Ignore result since buffer changed. This happens for slow language servers.
+
+  -- When use `autocmd CompleteDone <silent><buffer> lua vim.lsp.buf.signature_help()` to call signatureHelp handler
+  -- If the completion item doesn't have signatures It will make noise. Change to use `print` that can use `<silent>` to ignore
+  if not (result and result.signatures and result.signatures[1]) then
+    if config.silent ~= true then print('No signature help available') end
+    return show_signature()
+  end
+
+  local ft = vim.bo[ctx.bufnr].filetype
+  local triggers = vim.tbl_get(client.server_capabilities, 'signatureHelpProvider', 'triggerCharacters')
+
+  lines, hl = util.convert_signature_help_to_markdown_lines(result, ft, triggers)
+
+  if not lines or vim.tbl_isempty(lines) then
+    if config.silent ~= true then print('No signature help available') end
+    return show_signature()
+  end
+
+	if clients_no > 1 then table.insert(lines, 1, string.format('*%s*', client.name)) end
+  if stored_result then table.insert(stored_result, '---------') end
+
+  stored_result = stored_result and vim.list_extend(stored_result, lines) or lines
+  vim.api.nvim_buf_set_var(buf, method .. '_result', stored_result)
+
+  return show_signature()
+end
+
 M.setup = function(opts)
   vim.lsp.handlers['textDocument/hover'] = M.hover
-  vim.lsp.handlers['textDocument/definition'] = M.location_handler
+  vim.lsp.handlers['textDocument/definition'] = M.location
+  vim.lsp.handlers['textDocument/signatureHelp'] = M.signature_help
 
-  vim.keymap.set('n', ',w', ":w | =loadfile('~/projects/multi-lsp.nvim/lua/multi-lsp.lua')()<cr> | :mes clear", {})
+  vim.keymap.set('n', ',w', ":w | =loadfile(vim.fn.expand('%'))()<cr> | :mes clear", {})
 end
 
 M.setup()
-print('Loaded')
+
 return M
